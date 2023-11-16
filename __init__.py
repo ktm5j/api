02909b31cf839be4cac873fca0df5a5ba9ed1f03
Kate Miles <ktmiles2612@gmail.com>
2023-11-16 10:55:43 -0500

    Making progress
    Implemented GetIssues and CreateIssue
    
    Revoked api key I accidentally uploaded to github. Made constructor take in api
    key and endpoint url so this can be used with any github repo/api key.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,44 @@
 import requests
+import json
 
-endpoint = 'https://api.github.com/repos/ktm5j/gh-issues-connector/issues'
-headers = {
-    'Accept': 'application/vnd.github+json',
-    'Authorization': 'Bearer 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',
-    'X-GitHub-Api-Version': '2022-11-28'
-}
+from enum import Enum
 
-class GHConnector():
-    def GetIssues():
-        global endpoint, headers
-        r = requests.get(endpoint, headers=headers)
-        return r
-    
+class Status(Enum):
+    SUCCESS = 1
+    FAILURE = 2
+
+class IssuesConnector():
+    def __init__(self, _endpoint, _api_key):
+        self.endpoint = _endpoint
+        self.headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': f'Bearer {_api_key}',
+            'X-GitHub-Api-Version': '2022-11-28'
+        }
+
+    def IsWorking(self):
+        return
+
+    def GetIssues(self):
+        # global endpoint, headers
+        r = requests.get(self.endpoint, headers=self.headers)
+
+        if r.status_code != 200:
+            raise Exception("Request unsuccessful, check endpoint url or api key!")
+
+        return json.loads(r.text)
+
+    def CreateIssue(self, title, body, assignedto=None, labels=None):
+        payload = {'title': title,
+                   'body': body,
+                   'assignee': assignedto,
+                   'labels': labels
+                 }
+
+        r = requests.post(self.endpoint, headers=self.headers, data=json.dumps(payload))
+        # return r
+        if r.status_code != 201:
+            raise Exception("Request unsuccessful, check endpoint url or api key!")
+
+        return json.loads(r.text)
+
